0056ef838d253f181addcf3bfa3c5b72ac9efef6
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2020-10-10 18:29:06 +0100

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    "@snowpack/plugin-typescript"
+  ],
+};
